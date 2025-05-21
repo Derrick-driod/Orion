b5c935fddb6ca0b8ae8487173c4b9b4c89d58ed9
Derrick-driod <joyosayuki26@gmail.com>
2025-05-21 15:49:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,131 +1,25 @@
-import chalk from 'chalk'
-import { spawn } from 'child_process'
-import express from 'express'
-import figlet from 'figlet'
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url';
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
 
-figlet(
-  'TOHID KHAN',
-  {
-    font: 'Ghost',
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-  },
-  (err, data) => {
-    if (err) {
-      console.error(chalk.red('Figlet error:', err))
-      return
-    }
-    console.log(chalk.yellow(data))
-  }
-)
-
-figlet(
-  'Advanced Whatsapp Bot',
-  {
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-  },
-  (err, data) => {
-    if (err) {
-      console.error(chalk.red('Figlet error:', err))
-      return
-    }
-    console.log(chalk.magenta(data))
-  }
-)
-
-const app = express()
-const port = process.env.PORT || 5000
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'assets')));
-
-app.get('/', (req, res) => {
-  res.redirect('/global.html');
-});
-
-app.listen(port, () => {
-  console.log(chalk.green(`Port ${port} is open`))
-})
-
-let isRunning = false
-
-async function start(file) {
-  if (isRunning) return
-  isRunning = true
-
-  const currentFilePath = new URL(import.meta.url).pathname
-  const args = [path.join(path.dirname(currentFilePath), file), ...process.argv.slice(2)]
-  const p = spawn(process.argv[0], args, {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
-  })
-
-  p.on('message', data => {
-    console.log(chalk.cyan(`✔️RECEIVED ${data}`))
-    switch (data) {
-      case 'reset':
-        p.kill()
-        isRunning = false
-        start.apply(this, arguments)
-        break
-      case 'uptime':
-        p.send(process.uptime())
-        break
-    }
-  })
-
-  p.on('exit', code => {
-    isRunning = false
-    console.error(chalk.red(`❌Exited with code: ${code}`))
-
-    if (code === 0) return
-
-    fs.watchFile(args[0], () => {
-      fs.unwatchFile(args[0])
-      start('global.js')
-    })
-  })
-
-  p.on('error', err => {
-    console.error(chalk.red(`Error: ${err}`))
-    p.kill()
-    isRunning = false
-    start('global.js')
-  })
-
-  const pluginsFolder = path.join(path.dirname(currentFilePath), 'plugins')
-
-  fs.readdir(pluginsFolder, async (err, files) => {
-    if (err) {
-      console.error(chalk.red(`Error reading plugins folder: ${err}`))
-      return
-    }
-    console.log(chalk.yellow(`Installed ${files.length} plugins`))
-
-    try {
-      const { default: baileys } = await import('@whiskeysockets/baileys')
-      const version = (await baileys.fetchLatestBaileysVersion()).version
-      console.log(chalk.yellow(`Using Baileys version ${version}`))
-    } catch (e) {
-      console.error(chalk.red(' Baileys library is not installed'))
-    }
-  })
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
 }
-
-start('global.js')
-
-process.on('unhandledRejection', () => {
-  console.error(chalk.red(`Unhandled promise rejection. Bot will restart...`))
-  start('global.js')
-})
-
-process.on('exit', code => {
-  console.error(chalk.red(`Exited with code: ${code}`))
-  console.error(chalk.red(`Bot will restart...`))
-  start('global.js')
-})
+start()
